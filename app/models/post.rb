140783f04fc97457910b9e6cfba0e2d2fbb0277d
Piotr Pupczyk <piotrek@jakbyco.com>
2014-12-02 12:39:51 +0100

    add scopes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  before_save :publish
+  before_create :publish
+
+  scope :published_posts, -> { where(published: true) }
+  scope :desc, -> { order(created_at: :desc) }
 
   def publish
     self.published = true
