88e8f12c4c5335d04e88ae41f36b413e661cd42d
Piotr Pupczyk <piotrek@jakbyco.com>
2014-12-03 18:04:29 +0100

    add tags

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :description, :code, :published)
+      params.require(:post).permit(:title, :description, :code, :published, :tag_list)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   before_create :publish
 
+  acts_as_taggable
+
   scope :published_posts, -> { where(published: true) }
   scope :desc, -> { order(created_at: :desc) }
 
